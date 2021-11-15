e418ead03c1dd6293ebf5872fd551708849b69ea
TADAS DELININKAITIS <t4dam94@gmail.com>
2021-11-15 07:33:25 +0200

    4: minimalus atvaizdavimo testavimas
    clear
    git clear
    --help

---
+++ js/components/FurnitureGridComponent.js
@@ -0,0 +1,9 @@
+class FurnitureGridComponent {
+  constructor() {
+    this.init();
+  }
+  init = () => {
+    this.htmlElement = document.createElement("div");
+    this.htmlElement.innerHTML = "AŠ ponas Gridas";
+  };
+}
